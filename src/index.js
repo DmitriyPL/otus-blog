ede0ce7debb08769f46c2f0b6051594b03eb550f
Dmitriy Plotitsyn <plhome@yandex.ru>
2022-06-19 15:07:59 +0300

    setup project

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 // JS
-import './js/'
+import "./js/main";
 
 // SCSS
-import './assets/scss/main.scss'
+import "./assets/scss/main.scss";
 
 // CSS (example)
-// import './assets/css/main.css'
-
-// Vue.js
-window.Vue = require('vue')
-
-// Vue components (for use in html)
-Vue.component('example-component', require('./components/Example.vue').default)
-
-// Vue init
-const app = new Vue({
-  el: '#app'
-})
+import "./assets/css/main.css";
---
+++ webpackconfig/webpack.base.conf.js
@@ -0,0 +1,127 @@
+const path = require("path");
+const fs = require("fs");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname, "../src"),
+  dist: path.join(__dirname, "../dist"),
+  assets: "assets/",
+};
+
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .filter((fileName) => fileName.endsWith(".pug"));
+
+module.exports = {
+  externals: {
+    paths: PATHS,
+  },
+  entry: {
+    main: PATHS.src,
+    // module: `${PATHS.src}/your-module.js`,
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].[hash].js`,
+    path: PATHS.dist,
+    publicPath: "/",
+    clean: true,
+    environment: {
+      arrowFunction: false,
+    },
+  },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: "vendors",
+          test: /node_modules/,
+          chunks: "all",
+          enforce: true,
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/i,
+        loader: "pug3-loader",
+      },
+
+      {
+        test: /\.js$/i,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/i,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+
+  resolve: {
+    alias: {
+      "~": PATHS.src,
+    },
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin(),
+
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/[name].[hash].css`,
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+        {
+          from: `${PATHS.src}/${PATHS.assets}fonts`,
+          to: `${PATHS.assets}fonts`,
+        },
+        { from: `${PATHS.src}/static`, to: "" },
+      ],
+    }),
+
+    ...PAGES.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          template: `${PAGES_DIR}/${page}`,
+          filename: `./${page.replace(/\.pug/, ".html")}`,
+        })
+    ),
+  ],
+};

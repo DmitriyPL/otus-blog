3f17bb7adcbd8e068e0cb45165d314a92f9e3db1
Dmitriy Plotitsyn <plhome@yandex.ru>
2022-07-11 21:52:39 +0300

    after lint:fix

--- src/js/slider.js
+++ src/js/slider.js
@@ -34,109 +34,100 @@ function getIndex(currentInd, length) {
 }
 
 function setSliderSize(elem, sliderSize) {
-  if (sliderSize){
+  if (sliderSize) {
     const sliders = elem.querySelectorAll(".carousel-pl__item img");
     sliders.forEach((slide) => {
       slide.style.height = `${sliderSize.slideHieght}px`;
-      slide.style.width  = `${sliderSize.slideWidth}px`;
-    }); 
+      slide.style.width = `${sliderSize.slideWidth}px`;
+    });
   }
 }
 
 function addSlides(elem, slides) {
-
-  slides.forEach( (slide, index) => {
-
-    const div = document.createElement('div');
+  slides.forEach((slide, index) => {
+    const div = document.createElement("div");
     div.classList.add("carousel-pl__item");
-    if (index === 0){
+    if (index === 0) {
       div.classList.add("carousel-pl__active");
     }
 
-    const img = document.createElement('img');   
+    const img = document.createElement("img");
     img.src = slide;
-    img.alt = `${index+1} slide`;
+    img.alt = `${index + 1} slide`;
 
-    div.append(img); 
+    div.append(img);
     elem.append(div);
-
   });
 }
 
 function addButtonInSlider(elem, buttonType) {
-  
-  const btn = document.createElement('button');
-  btnType = (buttonType === "left") ? "prev" : "next"; 
+  const btn = document.createElement("button");
+  const btnType = buttonType === "left" ? "prev" : "next";
   btn.classList.add("carousel-pl__control-button", btnType);
 
-  const img = document.createElement('img');
+  const img = document.createElement("img");
   img.classList.add("carousel-pl__control-icon");
   img.src = `./assets/img/icon-${buttonType}.png`;
 
   btn.append(img);
   elem.append(btn);
 
-  btn.addEventListener("click", function(ev) {
+  btn.addEventListener("click", function (ev) {
     ev.preventDefault();
     onSliderClick(btn, elem);
   });
-
 }
 
 function Slider(slideWrapper, params) {
-
   if (!(slideWrapper instanceof HTMLElement)) {
     console.log("Container is not DOM element");
   }
 
-  const div = document.createElement('div');
+  const div = document.createElement("div");
   div.classList.add("carousel-pl__inner");
   slideWrapper.append(div);
 
   addButtonInSlider(div, "left");
   addSlides(div, params.slides);
-  addButtonInSlider(div, "right")
+  addButtonInSlider(div, "right");
   setSliderSize(div, params.sliderSize);
-
 }
 
 function addSlidersOnPage() {
+  const sliderList = [];
 
-  const sliderList= [];
+  const sl1 = document.getElementById("sl1");
+  const sl2 = document.getElementById("sl2");
 
-  const sl1 = document.getElementById('sl1');
-  const sl2 = document.getElementById('sl2');
-
-  params1 = {
+  const params1 = {
     sliderSize: {
       slideWidth: 400,
-      slideHieght: 400
+      slideHieght: 400,
     },
     slides: [
       "./assets/img/40-300x300-grayscale.jpg",
       "./assets/img/63-300x300-grayscale.jpg",
-      "./assets/img/972-300x300-grayscale.jpg"
+      "./assets/img/972-300x300-grayscale.jpg",
     ],
-  }
+  };
 
   const newSlider1 = new Slider(sl1, params1);
   sliderList.push(newSlider1);
 
-  params2 = {
+  const params2 = {
     sliderSize: {
       slideWidth: 200,
-      slideHieght: 200
+      slideHieght: 200,
     },
     slides: [
       "./assets/img/40-300x300-grayscale.jpg",
       "./assets/img/63-300x300-grayscale.jpg",
-      "./assets/img/972-300x300-grayscale.jpg"
+      "./assets/img/972-300x300-grayscale.jpg",
     ],
-  }
+  };
 
   const newSlider2 = new Slider(sl2, params2);
   sliderList.push(newSlider2);
-
 }
 
 addSlidersOnPage();
